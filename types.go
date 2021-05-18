022631f8cdc1d33999fe9d4b9dc596cf42aec18c
Wenfeng Wang <kalot.wang@gmail.com>
2021-05-18 15:36:58 +0800

    Use phala forked GSRPC

--- types.go
+++ types.go
@@ -1,7 +1,7 @@
 package events
 
 import (
-	"github.com/centrifuge/go-substrate-rpc-client/v3/types"
+	"github.com/Phala-Network/go-substrate-rpc-client/v3/types"
 )
 
 type BlockRewardInfo struct {
