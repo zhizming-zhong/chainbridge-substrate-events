99e4f5ed4c2a3a424c6795a3e7c09978c1bbd0de
Wenfeng Wang <kalot.wang@gmail.com>
2021-07-21 15:41:35 +0800

    add new events

--- events.go
+++ events.go
@@ -26,32 +26,23 @@ type BridgeTransferEvents struct {
 }
 
 type PhalaEvents struct {
-	Phala_CommandPushed          []EventCommandPushed          //nolint:stylecheck,golint
-	Phala_TransferToTee          []EventTransferToTee          //nolint:stylecheck,golint
-	Phala_TransferToChain        []EventTransferToChain        //nolint:stylecheck,golint
-	Phala_WorkerRegistered       []EventWorkerRegistered       //nolint:stylecheck,golint
-	Phala_WorkerUnregistered     []EventWorkerUnregistered     //nolint:stylecheck,golint
-	Phala_Heartbeat              []EventHeartbeat              //nolint:stylecheck,golint
-	Phala_Offline                []EventOffline                //nolint:stylecheck,golint
-	Phala_Slash                  []EventSlash                  //nolint:stylecheck,golint
-	Phala_WorkerStateUpdated     []EventWorkerStateUpdated     //nolint:stylecheck,golint
-	Phala_WhitelistAdded         []EventWhitelistAdded         //nolint:stylecheck,golint
-	Phala_WhitelistRemoved       []EventWhitelistRemoved       //nolint:stylecheck,golint
-	Phala_RewardSeed             []EventRewardSeed             //nolint:stylecheck,golint
-	Phala_WorkerMessageReceived  []EventWorkerMessageReceived  //nolint:stylecheck,golint
-	Phala_MinerStarted           []EventMinerStarted           //nolint:stylecheck,golint
-	Phala_MinerStopped           []EventMinerStopped           //nolint:stylecheck,golint
-	Phala_NewMiningRound         []EventNewMiningRound         //nolint:stylecheck,golint
-	Phala_PayoutMissed           []EventPayoutMissed           //nolint:stylecheck,golint
-	Phala_WorkerReset            []EventWorkerReset            //nolint:stylecheck,golint
-	Phala_PayoutReward           []EventPayoutReward           //nolint:stylecheck,golint
-	Phala_LotteryMessageReceived []EventLotteryMessageReceived //nolint:stylecheck,golint
-}
-
-type MiningStakingEvents struct {
-	MiningStaking_PendingStakeApplied []EventPendingStakeApplied //nolint:stylecheck,golint
-	MiningStaking_PendingUnstakeAdded []EventPendingUnstakeAdded //nolint:stylecheck,golint
-	MiningStaking_PendingStakeAdded   []EventPendingStakeAdded   //nolint:stylecheck,golint
+	Phala_EventGatekeeperAdded          []EventGatekeeperAdded          //nolint:stylecheck,golint
+	Phala_EventCoplingDownExpireChanged []EventCoplingDownExpireChanged //nolint:stylecheck,golint
+	Phala_EventMiningStarted            []EventMiningStarted            //nolint:stylecheck,golint
+	Phala_EventMiningStoped             []EventMiningStoped             //nolint:stylecheck,golint
+	Phala_EventMiningCleanup            []EventMiningCleanup            //nolint:stylecheck,golint
+	Phala_EventMinerBounded             []EventMinerBounded             //nolint:stylecheck,golint
+	Phala_EventMinerEnterUnresponsive   []EventMinerEnterUnresponsive   //nolint:stylecheck,golint
+	Phala_EventMinerExitUnresponive     []EventMinerEnterUnresponsive   //nolint:stylecheck,golint
+	Phala_EventMinerDeposited           []EventMinerDeposited           //nolint:stylecheck,golint
+	Phala_EventMinerWithdrawed          []EventMinerWithdrawed          //nolint:stylecheck,golint
+	Phala_EventPoolCreated              []EventPoolCreated              //nolint:stylecheck,golint
+	Phala_EventPoolCommissionSetted     []EventPoolCommissionSetted     //nolint:stylecheck,golint
+	Phala_EventPoolCapacitySetted       []EventPoolCapacitySetted       //nolint:stylecheck,golint
+	Phala_EventPoolWorkerAdded          []EventPoolWorkerAdded          //nolint:stylecheck,golint
+	Phala_EventDeposit                  []EventDeposit                  //nolint:stylecheck,golint
+	Phala_EventWithdraw                 []EventWithdraw                 //nolint:stylecheck,golint
+	Phala_EventWithdrawRewards          []EventWithdrawRewards          //nolint:stylecheck,golint
 }
 
 type KittiesEvents struct {
@@ -178,169 +169,121 @@ type EventLotteryOpenBox struct {
 	Topics     []types.Hash
 }
 
-// pallet phala
-type EventCommandPushed struct {
+// pallet phala: registry
+type EventGatekeeperAdded struct {
 	Phase  types.Phase
-	Arg0   types.AccountID
-	Arg1   types.U32
-	Arg2   types.Bytes
-	Arg3   types.U64
+	worker types.Bytes
 	Topics []types.Hash
 }
 
-type EventTransferToTee struct {
+// pallet phala: mining
+type EventCoplingDownExpireChanged struct {
 	Phase  types.Phase
-	Arg0   types.AccountID
-	Arg1   types.U128
+	period types.U64
 	Topics []types.Hash
 }
 
-type EventTransferToChain struct {
+type EventMiningStarted struct {
 	Phase  types.Phase
-	Arg0   types.AccountID
-	Arg1   types.U128
-	Arg2   types.U64
+	miner  types.AccountID
 	Topics []types.Hash
 }
 
-type EventWorkerRegistered struct {
+type EventMiningStoped struct {
 	Phase  types.Phase
-	Arg0   types.AccountID
-	Arg1   types.Bytes
-	Arg2   types.Bytes
+	miner  types.AccountID
 	Topics []types.Hash
 }
 
-type EventWorkerUnregistered struct {
+type EventMiningCleanup struct {
 	Phase  types.Phase
-	Arg0   types.AccountID
-	Arg1   types.Bytes
+	user   types.AccountID
 	Topics []types.Hash
 }
 
-type EventHeartbeat struct {
+type EventMinerBounded struct {
 	Phase  types.Phase
-	Arg0   types.AccountID
-	Arg1   types.U32
+	miner  types.AccountID
+	worker types.Bytes
 	Topics []types.Hash
 }
 
-type EventOffline struct {
+type EventMinerEnterUnresponsive struct {
 	Phase  types.Phase
-	Arg0   types.AccountID
+	miner  types.AccountID
 	Topics []types.Hash
 }
 
-type EventSlash struct {
+type EventMinerExitUnresponive struct {
 	Phase  types.Phase
-	Arg0   types.AccountID
-	Arg1   types.AccountID
-	Arg2   types.U128
-	Arg3   types.AccountID
-	Arg4   types.U128
+	miner  types.AccountID
 	Topics []types.Hash
 }
 
-type EventWorkerStateUpdated struct {
+type EventMinerDeposited struct {
 	Phase  types.Phase
-	Arg0   types.AccountID
+	miner  types.AccountID
+	amount types.U128
 	Topics []types.Hash
 }
 
-type EventWhitelistAdded struct {
+type EventMinerWithdrawed struct {
 	Phase  types.Phase
-	Arg0   types.Bytes
+	miner  types.AccountID
+	amount types.U128
 	Topics []types.Hash
 }
 
-type EventWhitelistRemoved struct {
+// pallet phala: stakepool
+type EventPoolCreated struct {
 	Phase  types.Phase
-	Arg0   types.Bytes
+	owner  types.AccountID
+	pid    types.U64
 	Topics []types.Hash
 }
 
-type EventRewardSeed struct {
-	Phase           types.Phase
-	BlockRewardInfo BlockRewardInfo
-	Topics          []types.Hash
+type EventPoolCommissionSetted struct {
+	Phase      types.Phase
+	pid        types.U64
+	commission types.U64
+	Topics     []types.Hash
 }
 
-type EventWorkerMessageReceived struct {
+type EventPoolCapacitySetted struct {
 	Phase  types.Phase
-	Arg0   types.AccountID
-	Arg1   types.Bytes
-	Arg2   types.U64
+	pid    types.U64
+	cap    types.U128
 	Topics []types.Hash
 }
 
-type EventMinerStarted struct {
+type EventPoolWorkerAdded struct {
 	Phase  types.Phase
-	Arg0   types.U32
-	Arg1   types.AccountID
+	pid    types.U64
+	worker types.Bytes
 	Topics []types.Hash
 }
 
-type EventMinerStopped struct {
+type EventDeposit struct {
 	Phase  types.Phase
-	Arg0   types.U32
-	Arg1   types.AccountID
+	pid    types.U64
+	user   types.AccountID
+	amount types.U128
 	Topics []types.Hash
 }
 
-type EventNewMiningRound struct {
+type EventWithdraw struct {
 	Phase  types.Phase
-	Arg0   types.U32
+	pid    types.U64
+	user   types.AccountID
+	amount types.U128
 	Topics []types.Hash
 }
 
-type EventPayoutMissed struct {
+type EventWithdrawRewards struct {
 	Phase  types.Phase
-	Arg0   types.AccountID
-	Arg1   types.AccountID
-	Topics []types.Hash
-}
-
-type EventWorkerReset struct {
-	Phase  types.Phase
-	Arg0   types.AccountID
-	Arg1   types.Bytes
-	Topics []types.Hash
-}
-
-type EventPayoutReward struct {
-	Phase  types.Phase
-	Arg0   types.AccountID
-	Arg1   types.U128
-	Arg2   types.U128
-	Arg3   PayoutReason
-	Topics []types.Hash
-}
-
-type EventLotteryMessageReceived struct {
-	Phase    types.Phase
-	Sequence types.U64
-	Topics   []types.Hash
-}
-
-// pallet mining-staking
-type EventPendingStakeApplied struct {
-	Phase  types.Phase
-	Topics []types.Hash
-}
-
-type EventPendingUnstakeAdded struct {
-	Phase  types.Phase
-	Arg0   types.AccountID
-	Arg1   types.AccountID
-	Arg2   types.U128
-	Topics []types.Hash
-}
-
-type EventPendingStakeAdded struct {
-	Phase  types.Phase
-	Arg0   types.AccountID
-	Arg1   types.AccountID
-	Arg2   types.U128
+	pid    types.U64
+	user   types.AccountID
+	amount types.U128
 	Topics []types.Hash
 }
 
